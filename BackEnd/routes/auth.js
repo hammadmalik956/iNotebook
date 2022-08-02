5bf405429fdd8369b094027944da614a5f2c3efd
hammadmalik956 <hammadhameed956@gmail.com>
2022-08-02 16:18:36 +0500

    Video 50 Completed

--- BackEnd/routes/auth.js
+++ BackEnd/routes/auth.js
@@ -1,27 +1,99 @@
-const express = require ('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
-
-// Create A user using : POST "/api/auth" Doesn't require Authentication
-router.post('/',[
-    body('name','Enter a valid Name: ').isLength({min:3}),
-    body('email','Enter a valid Email').isEmail(),
-    body('password','Password must be atleast 8 characters long').isLength({min: 8})
-],(req,res)=>{
+const JWT_SECRET = "Iama$good#boy";
+// Create A user using : POST "/api/auth/createuser" Doesn't require Authentication
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid Name: ").isLength({ min: 3 }),
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password must be atleast 8 characters long").isLength({
+      min: 8,
+    }),
+  ],
+  async (req, res) => {
+    // if there are errors , return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    // check whether the user with same email exist already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry the User with this email already exist" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
-      }).then(user => res.json(user))
-      .catch(err=>{console.log(err)
-      res.json({error: 'Please Enter a unique Value',message:err.message})})
+      });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      // res.json(user);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+//Authenticate a user using  : POST "/api/auth/login" Doesn't require Authentication
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    // if there are errors , return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-   
-})
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password,user.password);
+      
+      
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 module.exports = router;
